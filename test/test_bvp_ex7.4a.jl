d18b454123d9bd214b1f058141b5d7eb84af433d
Rob J Goedman <goedman@mac.com>
2016-04-13 09:10:51 -0700

    Updated conflict between f, f1 & f2

--- test/test_bvp_ex7.4a.jl
+++ test/test_bvp_ex7.4a.jl
@@ -4,11 +4,11 @@ old = pwd()
 ProjDir = Pkg.dir("NMfE", "examples", "NMfE", "Ch07", "BVP")
 cd(ProjDir)
 
-function f1(x::Float64, y::Vector{Float64})
+function f2(x::Float64, y::Vector{Float64})
   [y[2], 3x^2+4y[1]]
 end
 
-(iters, nsteps, converged, res) = shootingmethod(f1, [0.0, 0.0, 1.0, 1.0], [0.0, 1.0], 5)
+(iters, nsteps, converged, res) = shootingmethod(f2, [0.0, 0.0, 1.0, 1.0], [0.0, 1.0], 5)
 
 if !converged
   println("No conversion achieved.")
--- test/test_bvp_ex7.4b.jl
+++ test/test_bvp_ex7.4b.jl
@@ -4,11 +4,11 @@ old = pwd()
 ProjDir = Pkg.dir("NMfE", "examples", "NMfE", "Ch07", "BVP")
 cd(ProjDir)
 
-function f2(x::Float64, y::Vector{Float64})
+function f3(x::Float64, y::Vector{Float64})
   [y[2], -2y[2]/y[1]]
 end
 
-(iters, nsteps, converged, res) = shootingmethod(f2, [0.0, 1.0, 1.0, 2.0], [1.0, 3.0], 
+(iters, nsteps, converged, res) = shootingmethod(f3, [0.0, 1.0, 1.0, 2.0], [1.0, 3.0], 
   10, 0.000001, 80)
 
 if !converged
