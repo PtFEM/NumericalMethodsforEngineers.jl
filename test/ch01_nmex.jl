df1e93ca8fb068ffcc520d84392e7453edabb600
Rob J Goedman <goedman@mac.com>
2016-04-15 09:18:02 -0700

    Fixed nmex test

--- test/ch01_nmex.jl
+++ test/ch01_nmex.jl
@@ -6,7 +6,7 @@ x0 = [1., 1., 1.]
 tol = 1.0e-5
 limit = 100
 
-(iters, eigval, eigvec) = nmex(b, x0, tol, limit)
+(iters, eigval, eigvec) = nmex(b, x0; tol=tol, limit=100)
 eb = eig(b)
 
 @assert round(eigvec[:,1], 5) == round(eb[2][:,1], 5)
