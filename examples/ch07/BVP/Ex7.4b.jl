9c8e88e52359115a797907ed94087d2918b7bc8c
Rob J Goedman <goedman@mac.com>
2016-11-01 06:29:57 -0700

    Updates for Julia 0.5

--- examples/ch07/BVP/Ex7.4b.jl
+++ examples/ch07/BVP/Ex7.4b.jl
@@ -1,14 +1,13 @@
 using NMfE
 
-old = pwd()
-ProjDir = Pkg.dir("NMfE", "examples", "NMfE", "Ch07", "BVP")
-cd(ProjDir)
+ProjDir = dirname(@__FILE__)
+cd(ProjDir) #do
 
-function f(x::Float64, y::Vector{Float64})
-  [y[2], -2y[2]/y[1]]
-end
+  function f(x::Float64, y::Vector{Float64})
+    [y[2], -2y[2]/y[1]]
+  end
 
-res = shootingmethod(f, [0.0, 1.0, 1.0, 2.0], [1.0, 3.0], 10, 0.00001, 40)
-@show round(res, 4)
+  res = shootingmethod(f, [0.0, 1.0, 1.0, 2.0], [1.0, 3.0], 10, 0.00001, 40)
+  #@show round(res, 4)
 
-cd(old)
+  #end
