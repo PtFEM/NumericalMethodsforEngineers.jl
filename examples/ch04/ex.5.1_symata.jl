dd95e431cee8ac220024f1ddf3ff1044066aecb6
Rob J Goedman <goedman@mac.com>
2016-11-06 08:00:36 -0800

    Added Symata example

--- examples/ch04/ex.5.1_symata.jl
+++ examples/ch04/ex.5.1_symata.jl
@@ -1,11 +1,27 @@
-using NMfE, DataFrames, Plots
-pyplot(size=(700,700))
+using Symata, NMfE, DataFrames, Plots
+
+#=
+println("\u003D")
+
+xi = {1, 3, 6}
+yi = {1, 5, 10}
+L0(x_) := Simplify( Expand( ((x-xi[2])*(x-xi[3]))/((xi[1]-xi[2])*(xi[1]-xi[3])) ) )
+L1(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[3]))/((xi[2]-xi[1])*(xi[2]-xi[3])) ) )
+L2(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[2]))/((xi[3]-xi[1])*(xi[3]-xi[2])) ) )
+Q(x_) := Simplify(yi[1]*L0(x) + yi[2]*L1(x) + yi[3]*L2(x))
+
+Q(x)
+
+println("\u008")
+=#
 
 ProjDir = dirname(@__FILE__)
-cd(ProjDir) #do
+cd(ProjDir) do
   
-  x = [1.0, 3.0, 6.0, 5.0]
-  y = [1.0, 5.0, 10.0, 9.0]
+  pyplot(size=(700,700))
+  
+  x = [1.0, 3.0, 6.0]
+  y = [1.0, 5.0, 10.0]
   xi = [2.0, 4.5]
   (dfin, dfxi) = lagrangianpolynomial(length(x), x, y, xi)
   
@@ -29,4 +45,4 @@ cd(ProjDir) #do
   savefig("ex.5.1.png")
   gui()
   
-#end
+end
