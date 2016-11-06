6040b4feccacf51db7e8fc103eeacb4797152d29
Rob J Goedman <goedman@mac.com>
2016-11-06 11:03:11 -0800

    Prevent redefinition of lagrangianpolynomial if NMfE already loaded

--- examples/ch04/ex.5.1_symata.jl
+++ examples/ch04/ex.5.1_symata.jl
@@ -1,28 +1,30 @@
 using Symata, NMfE, DataFrames, Plots
 
-function lagrangianpolynomial(np::Int, x::Vector{Float64}, y::Vector{Float64}, xi::Vector{Float64})
-  local yi = Vector{Float64}(length(xi))
-  m = sort(hcat(x, y), 1)
-  for l in 1:length(xi)
-    for i in 1:np
-      term = 1.0
-      for j in 1:np
-        j !== i && (term *= (xi[l] - m[j, 1])/(m[i, 1] - m[j, 1]))
+if !isdefined(Main, :lagrangianpolynomial)
+  function lagrangianpolynomial(np::Int, x::Vector{Float64}, y::Vector{Float64}, xi::Vector{Float64})
+    local yi = Vector{Float64}(length(xi))
+    m = sort(hcat(x, y), 1)
+    for l in 1:length(xi)
+      for i in 1:np
+        term = 1.0
+        for j in 1:np
+          j !== i && (term *= (xi[l] - m[j, 1])/(m[i, 1] - m[j, 1]))
+        end
+        yi[l] += term * m[i, 2]
       end
-      yi[l] += term * m[i, 2]
     end
+    columns=[]
+    push!(columns, m[:,1])
+    push!(columns, m[:,2])
+    df = DataFrame(columns, [:x, :y])
+  
+    columns=[]
+    push!(columns, xi)
+    push!(columns, yi)
+    dfxi = DataFrame(columns, [:xi, :yi])
+  
+    (df, dfxi)
   end
-  columns=[]
-  push!(columns, m[:,1])
-  push!(columns, m[:,2])
-  df = DataFrame(columns, [:x, :y])
-  
-  columns=[]
-  push!(columns, xi)
-  push!(columns, yi)
-  dfxi = DataFrame(columns, [:xi, :yi])
-  
-  (df, dfxi)
 end
 
 #=
