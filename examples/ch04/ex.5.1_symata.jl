9010ce392e817f6806de1c1d3345d5587693af2d
Rob J Goedman <goedman@mac.com>
2016-11-08 09:59:01 -0800

    Switched to Symata master and added '*' as John suggested! Now working fine.

--- examples/ch04/ex.5.1_symata.jl
+++ examples/ch04/ex.5.1_symata.jl
@@ -29,19 +29,18 @@ if !isdefined(Main, :lagrangianpolynomial)
 end
 
 println()
-@Symata.ex begin
-  xi = {1, 3, 6}
-  yi = {1, 5, 10}
+@sym begin
+  xi = [1, 3, 6]
+  yi = [1, 5, 10]
   L0(x_) := Simplify( Expand( ((x-xi[2])*(x-xi[3]))/((xi[1]-xi[2])*(xi[1]-xi[3])) ) )
   L1(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[3]))/((xi[2]-xi[1])*(xi[2]-xi[3])) ) )
   L2(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[2]))/((xi[3]-xi[1])*(xi[3]-xi[2])) ) )
   Q(x_) := Simplify(yi[1]*L0(x) + yi[2]*L1(x) + yi[3]*L2(x))
-  SetJ(q, Q(x))
   SetJ(qs, ToString(Q(x)))
   Print("Q(x) = ", Q(x), "\n")
 end
 
-@Symata.ex Print("Q(4.5) = ", Q(4.5), "\n");
+@sym Print("Q(4.5) = ", Q(4.5), "\n");
 println("\n")
 
 ProjDir = dirname(@__FILE__)
--- test/ch04_symata.jl
+++ test/ch04_symata.jl
@@ -2,22 +2,23 @@ using Symata, Plots
 pyplot(size=(700,700))
 
 println()
-@Symata.ex begin
-  xi = {1, 3, 6}
-  yi = {1, 5, 10}
-  L0(x_) := Simplify( Expand( ((x-xi[2])(x-xi[3]))/((xi[1]-xi[2])(xi[1]-xi[3])) ) )
-  L1(x_) := Simplify( Expand( ((x-xi[1])(x-xi[3]))/((xi[2]-xi[1])(xi[2]-xi[3])) ) )
-  L2(x_) := Simplify( Expand( ((x-xi[1])(x-xi[2]))/((xi[3]-xi[1])(xi[3]-xi[2])) ) )
-  Q(x_) := Simplify(yi[1]L0(x) + yi[2]L1(x) + yi[3]*L2(x))
-  SetJ(q, Q(x))
-  SetJ(qs, ToString(Q(x)))
-  Print("Q(x) = ", Q(x))
+@sym begin
+  xi = [1, 3, 6]
+  yi = [1, 5, 10]
+  L0(x_) := Simplify( Expand( ((x-xi[2])*(x-xi[3]))/((xi[1]-xi[2])*(xi[1]-xi[3])) ) )
+  L1(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[3]))/((xi[2]-xi[1])*(xi[2]-xi[3])) ) )
+  L2(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[2]))/((xi[3]-xi[1])*(xi[3]-xi[2])) ) )
+  Q(x_) := Simplify(yi[1]*L0(x) + yi[2]*L1(x) + yi[3]*L2(x));
+  SetJ(q, ToString(Q(x)))
 end
 
-@Symata.ex Print("Q(4.5) = ", Q(4.5))
+@sym Print("Q(x) = ", Q(x))
+println()
+@sym Print("Q(4.5) = ", Q(4.5))
 
-q3=parse(qs)
-@eval f1(x) = $(q3)
+println()
+#q = @sym ToString(Q(x));
+@eval f1(x) = $(parse(q))
 
 xint = 1:0.1:6 # FloatRange
 p = plot(xint, f1.(xint), line=(:path, 1), label="interpolated curve")
