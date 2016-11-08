fe72edd07274a82781973553265f90f96aae6a5b
Rob J Goedman <goedman@mac.com>
2016-11-08 08:51:14 -0800

    Added (non-working?) test ch04_symata.jl

--- examples/ch04/ex.5.1_symata.jl
+++ examples/ch04/ex.5.1_symata.jl
@@ -1,13 +1,41 @@
 using Symata, NMfE, DataFrames, Plots
+pyplot(size=(700,700))
+
+if !isdefined(Main, :lagrangianpolynomial)
+  function lagrangianpolynomial(np::Int, x::Vector{Float64}, y::Vector{Float64}, xi::Vector{Float64})
+    local yi = Vector{Float64}(length(xi))
+    m = sort(hcat(x, y), 1)
+    for l in 1:length(xi)
+      for i in 1:np
+        term = 1.0
+        for j in 1:np
+          j !== i && (term *= (xi[l] - m[j, 1])/(m[i, 1] - m[j, 1]))
+        end
+        yi[l] += term * m[i, 2]
+      end
+    end
+    columns=[]
+    push!(columns, m[:,1])
+    push!(columns, m[:,2])
+    df = DataFrame(columns, [:x, :y])
+  
+    columns=[]
+    push!(columns, xi)
+    push!(columns, yi)
+    dfxi = DataFrame(columns, [:xi, :yi])
+  
+    (df, dfxi)
+  end
+end
 
 println()
 @Symata.ex begin
-  xi = {1, 3, 6};
-  yi = {1, 5, 10};
-  L0(x_) := Simplify( Expand( ((x-xi[2])*(x-xi[3]))/((xi[1]-xi[2])*(xi[1]-xi[3])) ) );
-  L1(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[3]))/((xi[2]-xi[1])*(xi[2]-xi[3])) ) );
-  L2(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[2]))/((xi[3]-xi[1])*(xi[3]-xi[2])) ) );
-  Q(x_) := Simplify(yi[1]*L0(x) + yi[2]*L1(x) + yi[3]*L2(x));
+  xi = {1, 3, 6}
+  yi = {1, 5, 10}
+  L0(x_) := Simplify( Expand( ((x-xi[2])*(x-xi[3]))/((xi[1]-xi[2])*(xi[1]-xi[3])) ) )
+  L1(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[3]))/((xi[2]-xi[1])*(xi[2]-xi[3])) ) )
+  L2(x_) := Simplify( Expand( ((x-xi[1])*(x-xi[2]))/((xi[3]-xi[1])*(xi[3]-xi[2])) ) )
+  Q(x_) := Simplify(yi[1]*L0(x) + yi[2]*L1(x) + yi[3]*L2(x))
   SetJ(q, Q(x))
   SetJ(qs, ToString(Q(x)))
   Print("Q(x) = ", Q(x), "\n")
@@ -21,8 +49,6 @@ q3=parse(qs)
 
 ProjDir = dirname(@__FILE__)
 cd(ProjDir) do
-  
-  pyplot(size=(700,700))
   
   x = [1.0, 3.0, 6.0]
   y = [1.0, 5.0, 10.0]
