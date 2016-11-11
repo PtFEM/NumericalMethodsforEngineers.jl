524d8bc36608f79cb648b36cb6fa06ab6424eba7
Rob J Goedman <goedman@mac.com>
2016-11-11 15:32:19 -0800

    Completed Vianello iterations

--- test/ch04_vianello_symata.jl
+++ test/ch04_vianello_symata.jl
@@ -3,9 +3,9 @@ using Symata
 @sym begin
   ClearAll(f, y0, a, l, x)
   y0(x_) := (e*x)*(l^3 - 2*l*x^2 + x^3)
-  #Println(y0(x))
   f(maxiters_, y0_) := Module([i, yder, res=Table(Table( 0.0 , [i,6]), [j,maxiters])],
     (
+      # Initialize the first row of the res matrix
       res[1,2] = y0(x),
       For(i=1, i<=4, Increment(i),
         [
@@ -13,35 +13,31 @@ using Symata
           res[1, 2+i] = yder(x)
         ]
       ),
+      # Now start the real loop to improve accuracy in subsequent rows,
+      # res[i, 1] contains the Euler ratio.
       For(i=2, i<=maxiters, Increment(i),
         [
-          res[i, 6] = Simplify(w/(EI)*(x-l)*res[i-1,4] + w/(EI)*res[i-1,3])
+          res[i, 6] = Simplify(w/(EI)*((x-l)*res[i-1,4] + res[i-1,3]))
           # Above the basic Euler equation for a pinned-pinned flagpole
-          ClearAll(K1, tmpy5, tmpy4)
+          ClearAll(K1, K11, tmpy5, tmpy4, tmp)
           tmpy5(x_) = Simplify(Integrate(res[i,6], x) + K1)
           tmpy4(x_) = Simplify(Integrate(tmpy5(x), x))
-          res[i,5] = Simplify(ReplaceAll( tmpy4(x), Solve(-tmpy4(l), K1)[1][1] ))
-          res[i,4] = Simplify(ReplaceAll( tmpy5(x), K11[1][1] ))
-          
-          ClearAll(K1, K11, tmpy3, tmpy2)
+          K11 = Solve(-tmpy4(l), K1)
+          res[i,5] = Simplify(ReplaceAll( tmpy5(x), K1 => K11[1][1][2] ))
+          res[i,4] = Simplify(ReplaceAll( tmpy4(x), K1 => K11[1][1][2] ))
+          ClearAll(K1, K11, tmpy3, tmpy2, tmp)
           tmpy3(x_) = Simplify(Integrate(res[i,4], x) + K1)
-          Println("tmpy3: ", tmpy3(x), "\n")
           tmpy2(x_) = Simplify(Integrate(tmpy3(x), x))
-          Println("tmpy2: ", tmpy2(x), "\n")
           K11 = Solve(-tmpy2(l), K1)
-          Println("K11[1][1]: ",K11[1][1], "\n")
-          res[i,3] = Simplify(ReplaceAll( tmpy3(x), K11[1][1] ))
-          res[i,2] = Simplify(ReplaceAll( tmpy2(x), K11[1][1] ))
+          res[i,3] = Simplify(ReplaceAll( tmpy3(x), K1 => K11[1][1][2] ))
+          res[i,2] = Simplify(ReplaceAll( tmpy2(x), K1 => K11[1][1][2] ))
           tmp = Simplify( (((l^3)*w)/EI)*(res[i-1,3]/res[i, 3]) )
-          Println("tmp: ", tmp, "\n")
           res[i,1] = N(Simplify(ReplaceAll(tmp, x => l)))
-          Println("N[res[i, 1]]: ", res[i,1], "\n")
+          Println("N[res[",i,", 1]]: ", res[i,1])
         ]
       ),
       Return(res)
     )
   )
-  #Println(f(2, y0(x)))
-  f(4, y0(x))
-  Null
+  Println(f(9, y0(x))[:, 1])
 end
