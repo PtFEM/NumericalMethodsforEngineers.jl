80b7693e7554b07a10e34a932ab6cb32dd6dece7
Rob J Goedman <goedman@mac.com>
2016-11-19 16:12:43 -0800

    Added examples 7.16a & b

--- examples/ch07/WRM/Ex.7.16.jl
+++ examples/ch07/WRM/Ex.7.16.jl
@@ -16,7 +16,7 @@ using Base.Test
   #Y(x_) := F(x) + C(a)*Ψ(x)
   #
   R(x_) = Simplify(D(Y(x), x, 2) - 3*x - 4*Y(x))
-  sol = Solve(R(xi[2]), a)
+  sol = Solve(Integrate(R(x), [x, 0, 1]), a)
   ytilde(x_) = Y(x) ./ Flatten(sol)
   SetJ(r, ToString(Simplify(R(x))))
   SetJ(s, "$(sol[1][1][2])")
@@ -27,13 +27,14 @@ println()
 @sym Println("\nY(x) = ", Y(x))
 @sym Println("R(x) = ", R(x))
 @sym Println("ytilde(x) = ", Simplify(ytilde(x)))
-println("Example 7.15 gives: ytilde = 1/6 * x * (7x - 1)")
+println("Example 7.16 gives: ytilde = 1/16*x*(21x - 5)")
 println()
 
 @eval rf(x, a) = $(parse(r))
 @eval a = $(parse(s))
 @eval ytilde(x) = $(parse(t))
 
+rf1(x) = rf(x, a)
 @assert r == "4.0 - 8.0a - 3x + 4x*(1.0 - 2.0x + 4.0a*(-1 + x))"
-@assert t == "x*(-0.16666666666666663 + 1.1666666666666665x)"
-@assert rf(0.5, a) < eps()
+@assert t == "x*(-0.3125 + 1.3125x)"
+@assert (quadgk(rf1, 0, 1))[1] < eps()
---
+++ examples/ch07/WRM/Ex.7.16b.jl
@@ -0,0 +1,40 @@
+using NMfE
+using Base.Test
+
+@sym begin
+  ClearAll(xi, yi, Y, Ydot, Ydotdot, ytilde1, ytilde2, R)
+  xi = [0, 1//3, 2//3, 1]
+  yi = [0, a, b, 1]
+  Y(x_) := lagrangepolynomial(xi, yi)
+  #
+  # Can be re-formulated as Y(x) = F(x) + C1(a) * Ψ1(x) + C2(a) * Ψ2(x)
+  #
+  # F(x_) := (1/2)*(9x^3 - 9x^2 + 2x)
+  # C1(a_) := (27/2)*(a-b)
+  # Ψ1(x_) := x^3 - x^2
+  # C2(a_) := -(9/2)*(2a-b)
+  # Ψ2(x_) := x^2 - x
+  #
+  # Y(x_) := F(x) + C1(a)*Ψ1(x) + C2(a)*Ψ2(x)
+  #
+  R(x_) = Simplify(D(Y(x), x, 2) - 3*x - 4*Y(x))
+  sol = Solve([Integrate(R(x), [x, 0, 0.5]), Integrate(R(x), [x, 0.5, 1])], [a, b])
+  ytilde(x_) = Simplify(Y(x) ./ Flatten(sol))
+  SetJ(r, ToString(Simplify(R(x))))
+  SetJ(s, "[$(sol[1][1][2]), $(sol[1][2][2])]")
+  SetJ(t, ToString(Simplify(ytilde(x))))
+end
+
+println()
+@sym Println("\nY(x): ", Y(x))
+@sym Println("R(x) = ", R(x))
+@sym Println("ytilde(x) = ", ytilde(x))
+println()
+
+@eval rf(x, a, b) = $(parse(r))
+@eval (a, b) = $(parse(s))
+
+rf1(x) = rf(x, a, b)
+@assert t == "x*(0.22596153846153857 - 0.30288461538461586x + 1.0769230769230769x^2)"
+@assert (quadgk(rf1, 0, 0.5))[1] < 5*eps()
+@assert (quadgk(rf1, 0.5, 1.0))[1] < 5*eps()
