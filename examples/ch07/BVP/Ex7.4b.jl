a57d6e8263cccec947b128816a5bb293e1a3800c
Rob J Goedman <goedman@mac.com>
2016-11-20 15:56:35 -0800

    Minor update

--- examples/ch07/BVP/Ex7.4b.jl
+++ examples/ch07/BVP/Ex7.4b.jl
@@ -1,7 +1,7 @@
 using NMfE
 
 ProjDir = dirname(@__FILE__)
-cd(ProjDir) #do
+cd(ProjDir) do
 
   function f(x::Float64, y::Vector{Float64})
     [y[2], -2y[2]/y[1]]
@@ -10,4 +10,4 @@ cd(ProjDir) #do
   res = shootingmethod(f, [0.0, 1.0, 1.0, 2.0], [1.0, 3.0], 10, 0.00001, 40)
   #@show round(res, 4)
 
-  #end
+end
