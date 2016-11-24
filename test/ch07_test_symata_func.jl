50a4e7d291e0da9b5e009655bb7bed3d90c66ff4
Rob J Goedman <goedman@mac.com>
2016-11-24 13:00:51 -0800

    Minor change

--- test/ch07_test_symata_func.jl
+++ test/ch07_test_symata_func.jl
@@ -1,34 +1,34 @@
 using Symata
 
-function symtest(n)
-    r = Symata.exfunc(parse("x^$n"))
-    #Symata.symprintln(r)
-    r
+function symtest1(n, f)
+  r = Symata.exfunc(parse("$(f)"))
+  Symata.setsymval(:rf, r)
+  r
 end
 
-r = symtest(3)
-println("r = $r")     
+r = symtest1(3, "x^n")
+println("r = $r") 
+    
+# Not quite sure how to convert r above into a Julia function.
+# If r is a String we can just parse:
 
-# Not quite sure how to convert r above to a Julia function.
-# Works great if I return a String that I can parse() in Julia:
-
-function symtest2(n)
-  @sym f(x_) := J(parse("(2x)^n"))
-  @sym SetJ(r1, ToString(f(x)))
+function symtest2()
+  @sym F(x_) := J(parse("(2x)^n"))
+  @sym SetJ(r1, ToString(F(x)))
 end
 
-symtest2(4)
-println("\nr1 = $r1\n")
+symtest2()
 @eval f1(x, n) = $(parse(r1))
 
 f1(5, 3) |> display
 println()
 
 # Now I want to pass in the string in J(parse("...")):
-
+#=
 function symtest3(n, f)
   Symata.setsymval(:fi, f)
-  Symata.exfunc(parse("ToString(Unpack(fi))"))    # ?????????
+  r = Symata.exfunc(parse("ToString(Unpack(fi))"))    # ?????????
+  r
 end
 
 r2 = symtest3(4, "(3x)^n")
@@ -46,8 +46,4 @@ fstr = """
     end
   )
 """
-r3 = symtest3(4, fstr)
-@eval f3(x, n) = $(parse(r3))
-
-f3(5, 3) |> display
-println()
+=#
