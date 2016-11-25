92959a61070d30eb90d08006b4fcfa23d919f975
Rob J Goedman <goedman@mac.com>
2016-11-24 18:05:18 -0800

    Added John's Symata in a Julia function example!

---
+++ test/ch07_jl_sym_fnc_ex.jl
@@ -0,0 +1,24 @@
+using NMfE
+
+setsymata(:xin, List(0.0,0.5,1.0))
+setsymata(:yin, List(0.0,0.6,1.0))
+
+f = @sym Compile(Evaluate(lagrangepolynomial(xin,yin)));
+
+println("\nf(0.75) = ", f(0.75))
+
+function lagrangefunc(x1,y1)
+    setsymata(:xin, List(x1...))
+    setsymata(:yin, List(y1...))
+    symeval(parse("Compile(Evaluate(lagrangepolynomial(xin,yin)))"))
+end
+
+f1 = lagrangefunc([0.0,0.5,1.0], [0.0,0.6,1.0])
+
+println("f1(0.75) = ", f1(0.75))
+
+xl1 = [0, 1/3, 2/3, 1]
+yl1 = [0, 0.3, 0.6, 1]
+f2 = lagrangefunc(xl1, yl1)
+
+println("f2(0.75) = ", f1(0.75))
