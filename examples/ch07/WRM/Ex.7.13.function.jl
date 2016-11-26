04552275395ffd67fd35e9fdc033a2e3eef1078f
Rob J Goedman <goedman@mac.com>
2016-11-26 15:43:05 -0800

    Added the function version (after reverting Symata back a few commits)

--- examples/ch07/WRM/Ex.7.13.function.jl
+++ examples/ch07/WRM/Ex.7.13.function.jl
@@ -1,30 +1,8 @@
 using NMfE
 using Base.Test
 
-#=
 @sym begin
-  ClearAll(xi, yi, Y, R, sol, ytilde)
-  xi = [0, 1//2, 1]
-  yi = [0, a, 1]
-  Y(x_) := LagrangePolynomial(xi, yi)
-  #
-  # Can be formulated as ytile(x) = F(x) + C1(a) * Ψ(x)
-  #
-  # F(x_) := 2*x^2 - x
-  # C(a_) := -4a
-  # Ψ(x_) := x^2 - x
-  #
-  #Y(x_) := F(x) + C(a)*Ψ(x)
-  #
-  R(x_) = Simplify(D(Y(x), x, 2) - 3*x - 4*Y(x))
-  R(x_) = Simplify(R(x) ./ (a => -C1/4))
-  SetJ(r, ToString(Simplify(R(x))))
-end
-=#
-
-@sym begin
-  CollocationWeightedResidualMethod(xi_, yi_) := Module(
-    [Y, R, ytilde],
+  CollocationWeightedResidualMethod(xi_, yi_) := Module([],
     begin
       Y(x_) := LagrangePolynomial(xi, yi)
       #
@@ -37,9 +15,12 @@ end
       #Y(x_) := F(x) + C(a)*Ψ(x)
       #
       R(x_) = Simplify(D(Y(x), x, 2) - 3*x - 4*Y(x))
+      Println("R(x) = $(R(x))")
       R(x_) = Simplify(R(x) ./ (a => -C1/4))
+      Println("R(x) = $(R(x))")
       SetJ(r, ToString(Simplify(R(x))))
-      Return(Simplify(R(x)))
+      Println("R(x) = $(R(x))")
+      Return(R(x))
     end
   )
 end
@@ -50,7 +31,7 @@ function wrmcollocation(x1,y1)
     symeval(parse("Compile(Evaluate(CollocationWeightedResidualMethod(xin,yin)))"))
 end
 
-f1 = wrmcollocation([0.0,0.5,1.0], [0.0,0.6,1.0])
+ytilde = wrmcollocation([0.0,0.5,1.0], [0.0,:a,1.0])
 
 println("\n\nExample 7.13: y'' = 3x + 4y, y(0)=0, y(1)=1")
 println("Residual for Weighted Residual Method using 1 point Lagragian Polynomial")
@@ -58,4 +39,4 @@ println("Residual for Weighted Residual Method using 1 point Lagragian Polynomia
 @sym Println("R(x) = ", R(x), "\n")
 println("( Example 7.14 gives: R = -4x^2*(2 + C1) + x*(1 + 4C1) + 2*(2 + C1) )", "\n")
 
-@assert r == "4.0 + 2.0C1 - 3x - 4x*(-1.0 + 2.0x + C1*(-1 + x))"
+@assert r == "4.0 + 2.0C1 - 3x - 4x*(-1.0 + 2.0x + C1*(-1.0 + x))"
