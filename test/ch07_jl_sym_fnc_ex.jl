543e2b76ac8b889480c9483060fc20cb7f382627
Rob J Goedman <goedman@mac.com>
2016-11-26 11:34:18 -0800

    Updated Symata function test , broken since: (INFO: Updating Symata master... 997239b7 → ce43aca2)?

--- test/ch07_jl_sym_fnc_ex.jl
+++ test/ch07_jl_sym_fnc_ex.jl
@@ -1,4 +1,27 @@
-using NMfE
+using Symata
+
+@sym begin
+  LagrangePolynomial(xi_, yi_) := Module([N, sum, num, den],
+    begin
+      N = Length(xi)
+      sum = 0
+      For( i=1, i <= N, Increment(i),
+        begin
+          num = 1.0
+          den = 1.0
+          For( j=1, j <= N, Increment(j),
+            begin
+              num = If(j != i, num = num * (x-xi[j]), num)
+              den = If(j != i, den = den * (xi[i]-xi[j]), den)
+            end
+          )
+          sum = sum + yi[i] * num/den
+        end
+      ),
+      Return(Simplify(sum))
+    end
+  )
+end
 
 setsymata(:xin, List(0.0,0.5,1.0))
 setsymata(:yin, List(0.0,0.6,1.0))
