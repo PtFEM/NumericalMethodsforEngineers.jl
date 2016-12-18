304484d5f6a0aff7306a7652112a17ffd4bf0273
Rob J Goedman <goedman@mac.com>
2016-12-18 09:34:55 -0700

    Updated ch08/ex01.jl

--- examples/ch08/ex01.jl
+++ examples/ch08/ex01.jl
@@ -3,9 +3,9 @@ using Base.Test
 
 @sym begin
   ClearAll(x, y, sol)
-  #sol1 = DSolve(D(y(t),t) - y(t) + Sin[t], y(t))
-  #Print("DSolve(D(y(t),t) - y(t) + 2*t, y, t) = $(sol1)\n")
-  sol2 = Roots(D(x^4+1,x,2),x)
-  Println(sol2)
+  sol1 = DSolve(D(y(t),t) - y(t) + Sin(t), y(t))
+  Print("DSolve(D(y(t),t) - y(t) + Sin(t), y(t)) = $(sol1)\n")
+  #sol2 = Roots(D(x^4+1,x,2),x)
+  #Println(sol2)
 end
 
