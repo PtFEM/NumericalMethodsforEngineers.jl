090c00e81c0ffd6bf15411a312e9bac8b6a8c1a3
Rob J Goedman <goedman@mac.com>
2017-03-31 10:57:33 -0600

    Removed Fortran programs

--- fortran/nm_lib/check.f95
+++
@@ -1,8 +0,0 @@
-LOGICAL FUNCTION check(x1,x0,tol)
-!---Checks Convergence of Two Scalars---
- USE precision; IMPLICIT NONE
-!---.TRUE. if converged; no update of x0---
- REAL(iwp),INTENT(IN)::x0,x1,tol
- check=.NOT.ABS(x1-x0)/ABS(x0)>tol
-RETURN
-END FUNCTION check
--- fortran/nm_lib/norm.f95
+++
@@ -1,6 +0,0 @@
-FUNCTION norm(x)
-!---Returns L2 Norm of Vector x---
- USE precision; IMPLICIT NONE
- REAL(iwp),INTENT(IN)::x(:); REAL(iwp)::norm
- norm=SQRT(SUM(x**2))
-END FUNCTION norm
