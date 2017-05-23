92d81fa971ec1beb6be66fba5ee64c9cf018abfb
Rob J Goedman <goedman@mac.com>
2017-05-22 19:28:02 -0700

    Fixed ch02_jacobi.jl

--- test/ch02_jacobi.jl
+++ test/ch02_jacobi.jl
@@ -10,17 +10,17 @@ x = Float64[1; 1; 1]
 
 maxiters = 100
 for i in 1:100
-  res = jacobi!(x, A, a, tol=1.0e-8, maxiter=1)
+  res = jacobi!(x, A, a, tol=1.0e-8, maxiter=1, log=true)
   if res[2].isconverged
     break
   end
 end
 
-@assert round(A * x, 5) == a
+@assert round.(A * x, 5) == a
 
 x = Float64[1; 1; 1]
 for i in 1:100
-  res = gauss_seidel!(x, A, a, tol=1.0e-8, maxiter=1)
+  res = gauss_seidel!(x, A, a, tol=1.0e-8, maxiter=1, log=true)
   if res[2].isconverged
     break
   end
@@ -31,10 +31,10 @@ end
 x = Float64[1; 1; 1]
 ω = 1.0
 for i in 1:100
-  res = ssor!(x, A, a, ω, tol=1.0e-8, maxiter=1)
+  res = ssor!(x, A, a, ω, tol=1.0e-8, maxiter=1,log=true)
   if res[2].isconverged
     break
   end
 end
 
-@assert round(A * x, 5) == a
+@assert round.(A * x, 5) == a
