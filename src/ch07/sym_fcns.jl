094f2bfec208152266a0f6387977fe92ee8a44be
Rob J Goedman <goedman@mac.com>
2018-07-24 07:50:17 -0600

    Updated Symata formulation of LagrangePolynomial (as John suggested)

--- src/ch07/sym_fcns.jl
+++ src/ch07/sym_fcns.jl
@@ -1,3 +1,6 @@
+# My formulation
+
+#=
 @sym begin
   LagrangePolynomial(xi_, yi_) := Module([N, sum, num, den],
     begin
@@ -20,3 +23,30 @@
     end
   )
 end
+=#
+
+# John's better formulation
+
+@sym begin
+  LagrangePolynomial(xi_, yi_) := Module([N, sum, num, den],
+    begin
+      N = Length(xi)
+      sum = 0
+      For( i=1, i <= N, Increment(i),
+        begin
+          num = 1
+          den = 1
+          For( j=1, j <= N, Increment(j),
+            begin
+              If(j != i, num *= (x-xi[j]))
+              If(j != i, den *= (xi[i]-xi[j]))
+            end
+          )
+          sum += yi[i] * num/den
+        end
+      ),
+      Return(Simplify(sum))
+    end
+  )
+end
+
