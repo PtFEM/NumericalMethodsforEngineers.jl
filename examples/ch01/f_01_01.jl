48a936977569d46e2a071fefee7e4595db217514
Rob J Goedman <goedman@mac.com>
2018-07-26 08:49:39 -0600

    Start working on examples and playing with .travis.yml

--- examples/ch01/f_01_01.jl
+++ examples/ch01/f_01_01.jl
@@ -1,4 +1,5 @@
 using NumericalMethodsforEngineers
+using Test
 
 a = [10. 5. 6.; 5. 20. 4.; 6. 4. 30.]
 x0 = [1., 1., 1.]
@@ -13,10 +14,10 @@ println("Largest eigen value:           $eigval")
 println("Corresponding eigen vector:    $eigvec")
 println()
 
-jeigval, jeigvec = eig(a);
+jeigval, jeigvec = eigen(a);
 
 println("\nEigen values:                   $jeigval")
 println("Corresponding eigen vectors:")
 jeigvec |> display
 
-@test round(maximum(jeigval), 3) == round(eigval, 3)
+@test round(maximum(jeigval), digits=3) == round(eigval, digits=3)
--- examples/ch01/f_01_02.jl
+++ examples/ch01/f_01_02.jl
@@ -1,4 +1,5 @@
 using NumericalMethodsforEngineers
+using Test
 
 b = [16. -24. 18.; 3. -2. 0.; -9. 18. -17.]
 x0 = [1., 1., 1.]
@@ -13,8 +14,11 @@ println("Largest eigen value:            $eigval")
 println("Corresponding eigen vector:     $eigvec")
 println()
 
-jeigval, jeigvec = eig(b);
+jeigval, jeigvec = eigen(b);
 
 println("\nEigen values:                   $jeigval")
 println("Corresponding eigen vectors:")
-jeigvec
+jeigvec |> display
+println()
+
+@test round(minimum(jeigval), digits=3) == round(eigval, digits=3)
